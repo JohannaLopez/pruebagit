8e382ea14286faaddb7137abe7a3e2034c3b5cce
JohannaLopez <jclopez@espol.edu.ec>
2021-05-21 16:46:39 -0500

    agregando funcion rostro

--- MainActivity.java
+++ MainActivity.java
@@ -541,5 +541,20 @@ public class MainActivity extends BaseActivity {
             }
         });
     }
+	
+	 private void reconoceRostro( int x, int y){
+        UtilidadesHelper.mensajeAlerta(this , getString(R.string.dialogos_cerrar_sesion), getString(R.string.cerrar_sesion_confirmacion, getString(R.string.app_name)))
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        cerrarSesionB2C();
+                    }
+                }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        }).show();
+    }
 
 }
